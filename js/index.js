c207c667b7c869797301186124f45e424844495b
hyebine <berryme015@gmail.com>
2024-08-12 19:17:21 +0900

    카카오뱅크 모작 상단  top-bar html,css,js 추가

---
+++ js/index.js
@@ -0,0 +1,35 @@
+
+const dropdownBox = document.querySelector('.dropdown-box');
+const dropdownMenu = document.querySelector('.dropdown-menu');
+const dropdownBtn = document.querySelector('.dropdown-btn');
+const gnbli = document.querySelectorAll('.menu-gnb > li');
+
+
+dropdownBox.addEventListener('click', function (e) {
+  dropdownMenu.classList.toggle('act');
+  dropdownBtn.classList.toggle('act');
+
+  e.stopPropagation();
+
+});
+
+
+document.addEventListener('click', function (e) {
+  // 클릭된 요소가 dropdown-box 내부에 속하지 않는 경우
+  if (!dropdownBox.contains(e.target)) {
+    dropdownMenu.classList.remove('act');
+    dropdownBtn.classList.remove('act');
+  }
+});
+
+
+
+gnbli.forEach(i => {
+  i.addEventListener('mouseenter', function () {
+    document.querySelector('.top-bar').classList.add('showback');
+  });
+
+  i.addEventListener('mouseleave', function () {
+    document.querySelector('.top-bar').classList.remove('showback');
+  });
+});
